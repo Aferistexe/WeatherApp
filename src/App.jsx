b5a5c7a695912f618337f02720a11142f7c56ca7
Aferistexe <andrey.ivanov377@gmail.com>
2025-11-21 03:13:36 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,60 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function Money(){
-const [money,setMoney] = useState(JSON.parse(localStorage.getItem('balance'))||0);
-const [moneyValue, setValueMoney] = useState('')
-const [addoperationmoney,setaddOperationMoney] = useState( JSON.parse(localStorage.getItem('addsum')) ||[])
-const [removeoperationmoney,setremoveOperationMoney] = useState(JSON.parse(localStorage.getItem('removesum'))|| [])
 
-useEffect(() =>{
-  localStorage.setItem('addsum',JSON.stringify(addoperationmoney))
-},[addoperationmoney]
-)
+function Todolist() {
 
-useEffect(() => {
-  localStorage.setItem('removesum',JSON.stringify(removeoperationmoney))
-},[removeoperationmoney])
+  const [todos, setTodos] = useState([]);
+  const [valueTodo,setValuetodo] = useState('')
 
 
-useEffect(()=>{
-  localStorage.setItem('balance',money)
-},[money])
-const addMoney = () =>{
-  if(errorNan()){
-    alert("Введи число")
-  }else {
-    const numbersValue = Number(moneyValue)
-    setMoney(money+numbersValue)
-    setaddOperationMoney([...addoperationmoney,numbersValue])
-    setValueMoney('')
+  const addTask= () =>{
+    setValuetodo(valueTodo)
+    setTodos([...todos, {text:valueTodo,completed:false}])
+    setValuetodo("")
+    
   }
-  console.log(addoperationmoney)
+  const removeTask= (index) =>{
+    const array = [...todos]
+    array.splice(index,1);
+    setTodos(array)
+  }
 
+  const togglecomplited= (index) =>{
+    let array = todos.map((todo,i)=>{
+      if(i === index){
+        return {...todo,completed:!todo.completed}
+      }else{return todo }
+    })
+    setTodos(array)
+  }
+
+
+ 
   
-}
 
-const removeMoney = () => {
-  const numbersValue = Number(moneyValue)
-
-  if( money-numbersValue < 0){
-    alert("Недостаточно средств")
-    return
-  }else if(errorNan()){
-    alert("Введи число")
-  }
-  else{
-    setMoney(money-numbersValue)
-    setremoveOperationMoney([...removeoperationmoney,numbersValue])
-    setValueMoney("")
-  }
-
-}
-const errorNan = () =>{
-  if(moneyValue === '' || isNaN(Number(moneyValue))){
-    return true
-  }else{return false}
-}
-const resetHistory = () =>{
-  setaddOperationMoney([])
-  setremoveOperationMoney([])
-  setMoney(0)
-
-}
-
-const sumPlusMoney = () =>{
-  let sum = 0
-  for(let i = 0;i < addoperationmoney.length; i++){
-    sum += addoperationmoney[i]
-  }
-  return sum
-}
-const sumMinusMoney = () =>{
-  let sum = 0
-  for(let i = 0;i < removeoperationmoney.length; i++){
-    sum += removeoperationmoney[i]
-  }
-  return sum
-}
-
-
-
-return(
-
-  <div style={{}}>
-    <input type="text" value={moneyValue} onChange={(e) => setValueMoney(e.target.value)}/>
-    <button onClick={addMoney}>Доход</button>
-    <button onClick={removeMoney}>рассход</button>
-    <button onClick={resetHistory}>Очистить</button>
-      <div style={{display:'flex',justifyContent:'center', gap:"20px"}}>
-        <p>Баланс: {money}</p>
-        <p >Доход: {sumPlusMoney()}</p>
-        <p >Рассход: {sumMinusMoney()}</p>
-      </div>
-    <div style={{display:'flex',justifyContent:'center',gap:'30px'}}>
+  return(
+    <div>
+      <input type="text" value={valueTodo} onChange={(e) => setValuetodo(e.target.value)}/>
+      <button onClick={addTask}>Добавить</button>
       <div >
-          {addoperationmoney.map((m,i)=>(
-            <div key={i} style={{display:'flex',border:"1px solid green",padding:"10px",width:'100%',maxWidth:"600px"}}>
-              <span>{"+" +  m + " рублей"}</span>
-            </div>
-          )
-          )}
-      </div>
-      <div>
-        { removeoperationmoney.map((m,i)=> (
-          <div key={i} style={{display:'flex',border:'1px solid red',padding:"10px",width:'100%',maxWidth:"600px"}}>
-            <span>{"-" +  m + ' рублей'}</span>
-          </div>
-        ))}
-      </div>
+        <ol style={{display:'flex',flexDirection:'column',alignItems:"center",justifyContent:'center',gap:'30px'}} >
+          {todos.map((todo, i) => (
+            <li key={i} className={todo.completed ? 'textzacherk' : ''}>
+              <input type="checkbox" name="" id="" checked={todo.completed} onChange={(e) => togglecomplited(i)}/>
+              {todo.text}
+              <button onClick={()=> removeTask(i) }>Удалить</button>
+            </li>
+          ))}
+      </ol>
+</div>
     </div>
-  </div>
-  
-)
+  )
 }
 function App() {
   return(
     <div className='App'>
-      <Money/>
+      <Todolist/>
     </div>
   )
 }
